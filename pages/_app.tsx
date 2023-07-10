7afcf72b014a114fa01cc3f1da9dc0e49c81b5a6
yunusbahtiar22 <aboomarmoshab22@gmail.com>
2023-07-10 08:04:51 +0700

    setup tanstack/react-query ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,28 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: "light",
-      }}>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            colorScheme: "light",
+          }}>
+          <Component {...pageProps} />
+        </MantineProvider>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
