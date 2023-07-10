a2ec1f160faa540c1b4328c279379fbaa509c345
yunusbahtiar22 <aboomarmoshab22@gmail.com>
2023-07-10 11:37:04 +0700

    configure image host

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "hsi-sandbox.vercel.app",
+        pathname: "/image/**",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig
