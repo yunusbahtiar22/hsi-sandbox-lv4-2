5139373a9db5cd3bb12884358ea1585f1e7d9e7c
yunusbahtiar22 <aboomarmoshab22@gmail.com>
2023-07-11 15:25:36 +0700

    setup global font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
 import {
@@ -7,6 +6,7 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import { useState } from "react";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
@@ -19,6 +19,27 @@ export default function App({ Component, pageProps }: AppProps) {
           withNormalizeCSS
           theme={{
             colorScheme: "light",
+            components: {
+              Container: {
+                defaultProps: {
+                  sizes: {
+                    md: 1140,
+                  },
+                },
+              },
+            },
+            colors: {
+              warmPink: [
+                "#F2B2C3",
+                "#FF5480",
+                "#DA2755",
+                "#93314A",
+                "#65303D",
+                "#472A32",
+                "#332327",
+                "#251C1F",
+              ],
+            },
           }}>
           <Component {...pageProps} />
         </MantineProvider>
